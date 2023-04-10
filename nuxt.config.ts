10b73608717c2a95dc365ad24d6bdd9318a39482
Steve <ycsteven.tsao@gmail.com>
2023-04-11 06:14:36 +0800

    chore: use scss global vairable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['assets/css/global.scss'],
+  css: ['assets/scss/global.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "~/assets/scss/_variables.scss";',
+        },
+      },
+    },
+  },
 });
--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -2,3 +2,8 @@
   <p>id: {{ $route.params.id }}</p>
   <NuxtLink to="/">Index Page</NuxtLink>
 </template>
+<style scoped lang="scss">
+p {
+  color: $linkColor;
+}
+</style>
