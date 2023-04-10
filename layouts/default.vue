2fc9c3c6ec04510dd2d6de3b8aaa75eec861dac4
Steve <ycsteven.tsao@gmail.com>
2023-04-10 23:38:32 +0800

    add avatar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <nav>導航欄</nav>
+    <nav>
+      導航欄
+      <img class="avatar" src="~/assets/avatar/1.png" alt="avatar" />
+    </nav>
     <slot />
   </div>
 </template>
+
+<style scoped>
+.avatar {
+  width: 50px;
+  border: 1px solid rgb(218, 218, 218);
+  border-radius: 50%;
+}
+</style>
