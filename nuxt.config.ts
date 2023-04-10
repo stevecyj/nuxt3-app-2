586f3ab06b927b3677cbeb12d084571bc89ea446
Steve <ycsteven.tsao@gmail.com>
2023-04-11 06:40:23 +0800

    chore: set tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['assets/scss/global.scss'],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "~/assets/scss/_variables.scss";',
-        },
-      },
-    },
-  },
+  modules: ['@nuxtjs/tailwindcss'],
 });
