a9317b9b509e78c58d3f9f1f9f35357b7c7ac55c
Steve <ycsteven.tsao@gmail.com>
2023-04-12 06:42:29 +0800

    feat: refactor post detail page to handle async data loading
    - Remove the `title` and `content` variables from `pages/detail.vue`
    - Add a loading state to `pages/detail/[id].vue`
    - Display the post title and content in `pages/detail/[id].vue`
    - Move the script to fetch post data into `pages/detail/[id].vue`
    - Add a `pending` variable to `pages/detail/[id].vue` to handle async data loading
    
    Signed-off-by: Steve <ycsteven.tsao@gmail.com>

--- pages/detail.vue
+++ pages/detail.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="p-5">
-    <h1 class="text-2xl">{{ title }}</h1>
-    <div v-html="content"></div>
     <NuxtPage></NuxtPage>
   </div>
 </template>
--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="p-5">
+    <div v-if="pending">Loading ...</div>
+    <div v-else>
+      <h1 class="text-2xl">{{ data?.title }}</h1>
+      <div v-html="data?.content"></div>
+    </div>
+  </div>
   <p>id: {{ $route.params.id }}</p>
   <NuxtLink to="/">Index Page</NuxtLink>
 </template>
@@ -7,3 +14,8 @@ p {
   color: var(--link-color);
 }
 </style>
+<script setup lang="ts">
+const router = useRoute();
+const fetchPost = () => $fetch(`/api/detail/${router.params.id}`);
+const { data, pending } = await useAsyncData(fetchPost);
+</script>
