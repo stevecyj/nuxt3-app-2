3964b4f17659fb0b85158163c722c20d7e1c7209
stevecyj <ycsteven.tsao@gmail.com>
2023-04-11 17:18:27 +0800

    feat: request article data

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>Index Page</div>
-  <div>
-    <NButton>Hello</NButton>
+  <div class="flex items-center flex-col gap-2 py-4">
+    <h1>Index Page</h1>
+    <div>
+      <NButton>Hello</NButton>
+    </div>
+    <div>
+      <van-button type="primary" @click="showToast('toast')">button</van-button>
+      <VanButton type="success" @click="showNotify('notify')">button</VanButton>
+      <LazyVanButton type="default">lazy button</LazyVanButton>
+    </div>
+    <div>
+      <NuxtLink to="/detail/1">Detail Page</NuxtLink>
+      |
+      <NuxtLink to="/hello">Hello Nuxt3</NuxtLink>
+    </div>
+    <div v-for="post in posts" :key="post.id">
+      <NuxtLink class="text-lg" :to="`/detail/${post.id}`">{{
+        post.title
+      }}</NuxtLink>
+      <p class="text-slate-500">發佈於： {{ post.date }}</p>
+    </div>
   </div>
-  <div>
-    <van-button type="primary" @click="showToast('toast')">button</van-button>
-    <VanButton type="success" @click="showNotify('notify')">button</VanButton>
-    <LazyVanButton type="default">lazy button</LazyVanButton>
-  </div>
-  <NuxtLink to="/detail/1">Detail Page</NuxtLink>
-  |
-  <NuxtLink to="/hello">Hello Nuxt3</NuxtLink>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const posts = await $fetch('/api/posts');
+</script>
