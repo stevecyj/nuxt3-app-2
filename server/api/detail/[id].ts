6503a4099050a1298290eb635f274a699799be1f
stevecyj <ycsteven.tsao@gmail.com>
2023-04-11 17:53:39 +0800

    WIP: api params

--- server/api/detail/[id].ts
+++ server/api/detail/[id].ts
@@ -10,5 +10,16 @@ const postDir = path.join(process.cwd(), 'content');
 export default defineEventHandler(async (event) => {
   const fileName = getRouterParam(event, 'id') + '.md';
 
-  return { fileName };
+  // 取得文章內容
+  const fullPath = path.join(postDir, fileName);
+  const fileContent = fs.readFileSync(fullPath, 'utf-8');
+
+  // 從文章內容解析頁面資訊
+  const matterInfo = matter(fileContent);
+
+  // markdown to html
+  const processedContent = await remark().use(html).process(matterInfo.content);
+  const content = processedContent.toString();
+
+  return { title: matterInfo.data.title, content };
 });
