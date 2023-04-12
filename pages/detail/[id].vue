b068d4eedbb4202523278f835308a7b2b03f2c93
stevecyj <ycsteven.tsao@gmail.com>
2023-04-12 11:53:10 +0800

    feat: refactor and add new files for improved organization
    - Rename `composables/Counter.ts` to `composables/counter.ts`
    - Add new file `composables/user.ts`
    - Add comment section to `pages/detail/[id].vue`
    - Add login page `pages/login.vue`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -4,6 +4,16 @@
     <div v-else>
       <h1 class="text-2xl">{{ data?.title }}</h1>
       <div v-html="data?.content"></div>
+      <!--      評論區-->
+      <div class="py-2">
+        <van-field
+          v-model="value"
+          type="textarea"
+          autosize
+          placeholder="輸入評論"
+        ></van-field>
+        <van-button @click="onSubmit">送出</van-button>
+      </div>
     </div>
   </div>
   <p>id: {{ $route.params.id }}</p>
@@ -15,7 +25,21 @@ p {
 }
 </style>
 <script setup lang="ts">
-const router = useRoute();
-const fetchPost = () => $fetch(`/api/detail/${router.params.id}`);
+const route = useRoute();
+const fetchPost = () => $fetch(`/api/detail/${route.params.id}`);
 const { data, pending } = await useAsyncData(fetchPost);
+
+// 登錄狀態判斷能否評論
+const value = useState('comment', () => '');
+const isLogin = useLogin();
+const router = useRouter();
+const onSubmit = () => {
+  if (isLogin.value) {
+    // 提交留言...
+    value.value = '';
+  } else {
+    // 要求登录
+    router.push('/login?callback=' + route.path);
+  }
+};
 </script>
---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <van-button @click="onLogin">登入</van-button>
+  </div>
+</template>
+<script setup lang="ts">
+const isLogin = useLogin();
+const router = useRouter();
+const route = useRoute();
+const onLogin = () => {
+  isLogin.value = true;
+  const callback = route.query.callback?.toString() || '';
+  router.push(callback);
+};
+</script>
