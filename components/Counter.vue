00f5ada2f50b50fa209e6fdd9f3d11ae95e531d2
stevecyj <ycsteven.tsao@gmail.com>
2023-04-12 10:27:22 +0800

    feat: add counter increment/decrement buttons to Counter component
    - Add two buttons to increment and decrement the counter in the Counter component.
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="p-4">Counter: {{ counter }}</div>
+  <div class="mt-2">
+    <van-button @click="counter++">+</van-button>
+    <van-button @click="counter--">-</van-button>
+  </div>
 </template>
 <script setup lang="ts">
 const counter = useState('counter', () => Math.round(Math.random() * 1000));
