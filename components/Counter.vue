66df09b43417971810bdb86f301145396eecb1b7
stevecyj <ycsteven.tsao@gmail.com>
2023-04-12 10:34:30 +0800

    feat: refactor counter component to use reactive references
    - Add a new div to display counterRef value
    - Change the increment and decrement buttons to include both counters
    - Add a new reactive reference for counterRef
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="p-4">Counter: {{ counter }}</div>
+  <div class="p-4">CounterRef: {{ counterRef }}</div>
   <div class="mt-2">
-    <van-button @click="counter++">+</van-button>
-    <van-button @click="counter--">-</van-button>
+    <van-button
+      @click="
+        counter++;
+        counterRef++;
+      "
+      >+</van-button
+    >
+    <van-button
+      @click="
+        counter--;
+        counterRef--;
+      "
+      >-</van-button
+    >
   </div>
 </template>
 <script setup lang="ts">
 const counter = useState('counter', () => Math.round(Math.random() * 1000));
+const counterRef = ref(Math.round(Math.random() * 1000));
 </script>
