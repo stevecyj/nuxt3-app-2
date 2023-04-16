28b7c607d7a1267c094010d5dcd1a9d6e2060029
Steve <ycsteven.tsao@gmail.com>
2023-04-17 07:50:10 +0800

    feat: refactor user authentication flow
    - Add email input field and login button to login page
    - Move `getUserByEmail` and `createUser` functions to separate repository file
    - Add `login.ts` file to handle login API endpoint
    - Add `client.ts` file to initialize and export Prisma client
    - No changes were made to `IUser` interface
    
    Signed-off-by: Steve <ycsteven.tsao@gmail.com>

--- pages/login.vue
+++ pages/login.vue
@@ -5,17 +5,33 @@ const store = useUser()
 const { isLogin } = storeToRefs(store)
 const router = useRouter()
 const route = useRoute()
+const email = useState(() => '')
+
 function onLogin() {
-  isLogin.value = true
-  const callback = route.query.callback?.toString() || ''
-  router.push(callback)
+  // isLogin.value = true
+  // const callback = route.query.callback?.toString() || ''
+  // router.push(callback)
+
+  $fetch('/api/login', {
+    method: 'post',
+    body: {
+      email: email.value,
+    },
+  })
+    .then((user) => {
+      console.log(user)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 </script>
 
 <template>
   <div>
-    <van-button @click="onLogin">
-      登入
-    </van-button>
+    <NInput v-model:value="email" />
+    <NButton @click="onLogin">
+      登录
+    </NButton>
   </div>
 </template>
