c2a020d66c4bcd7bf93043ab38103ff13f6f3b91
Steve <ycsteven.tsao@gmail.com>
2023-04-17 01:03:51 +0800

    feat: create and include posts in user query operation
    - Add a create operation for users and posts
    - Include posts in the query for all users
    
    Signed-off-by: Steve <ycsteven.tsao@gmail.com>

--- server/database/test.ts
+++ server/database/test.ts
@@ -3,8 +3,24 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+  // insert data
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'JOJO',
+  //     email: 'jojo@qq.com',
+  //     posts: {
+  //       create: {
+  //         title: '10速通下一代ORM解決方案：Prisma',
+  //       },
+  //     },
+  //   },
+  // })
   // 查询所有用户
-  const allUsers = await prisma.user.findMany()
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
 
   console.log(allUsers)
 }
